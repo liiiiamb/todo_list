b77a968d332bf7f4615b269001fee382c00a9d15
Liam Boyd <notliamb@outlook.com>
2024-02-26 15:23:18 +0000

    Developed bloc code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
+enum TodoEvent {add, remove, toggle}
+
+class Todo{
+  final String title;
+  final bool completed;
+
+  Todo({required this.title, this.completed = false});
+}
+
+class TodoState{
+  final List<Todo> items;
+
+  TodoState({required this.items});
+  
+}
+
+class TodoBloc extends Bloc<TodoEvent, TodoState>{
+  @override
+  TodoState get initialState => TodoState(items: []);
+  
+  @override
+  Stream<TodoState> mapEventToState(TodoEvent event) async* {
+    if (event == TodoEvent.add){
+      yield TodoState(items: List.from(state.items)..add(Todo(title: 'New item'))); //UP TO HERE, POINT 7.
+    }
+  }
+}
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -10,10 +37,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Text('Page Test'),
+        // appBar: AppBar(
+        //   title: const Text('Material App Bar'),
+        // ),
+        body: const Text('To-Do List',
+        style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),),
         ),
       
     );
